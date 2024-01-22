881bfd7680d063e39ca5dfe89f3f17448623cc0d
Dennis Perrett <dennisperrett@Denniss-MacBook-Pro-3.local>
2024-01-22 16:25:19 +0100

    updated model. Solved convergence issues

--- main.R
+++ main.R
@@ -1,3 +1,4 @@
+rm(list=ls())
 setwd("/Users/dennisperrett/Documents/Uni/Semester 5/Research Seminar/simulation_study_DSEM")
 
 library(mvtnorm)
@@ -7,7 +8,7 @@ Sys.setenv(LANG = "en")
 source("gen_data_version02.R")
 ##########################
 phi0 <- diag(1)*.7+.3 # cov(eta) # Per latent factor
-mu0  <- c(0)          # mean(eta). # Per latent factor
+mu0  <- c(0)          # mean(eta). # Per Observed(?) factor
 ar0  <- c(.5)         # ar(1) structure # Per Latent Factor
 
 # Factor loadings must conform with means and td (the conditional variance of the items)
@@ -37,23 +38,21 @@ data <- list(
 
 # Initial values
 initial_values <- list(
-                      
-                       list(alpha = matrix(rep(0,N*J),N,J, byrow=T), ly = rep(0,J), ar = rep(0,N)),
-                       list(alpha = matrix(rep(2,N*J),N,J, byrow=T), ly = rep(2,J), ar = rep(2,N)),
-                       list(alpha = matrix(rep(1,N*J),N,J, byrow=T), ly = rep(1,J), ar = rep(1,N))
-                      
-                       
-                       )
+  list(alpha = matrix(rnorm(N*J,0,1),N,J, byrow=T), ly = rnorm(J,0,1), ar = rnorm(N,0,2)),
+  list(alpha = matrix(rnorm(N*J,0,3),N,J, byrow=T), ly = rnorm(J,0,3), ar = rnorm(N,0,1))
+)
 
 params <- c("alpha","ly","ar")
 
 #Note: Check working directory to find the model.file.
-fit.fa <- jags(data, inits=initial_values, params, model.file="model1.txt", n.chains=3, n.iter=4500,
-               n.burnin = 100, n.thin=1)
+fit.fa <- jags(data,  parameters.to.save=params, model.file="model1.txt", n.chains=7, n.iter=8000,
+               n.burnin = 100, n.thin=15)
 
 fit.fa
 
-traceplot(fit.fa)
+
+
+
 
 mean(fit.fa$BUGSoutput$sims.list$alpha)
 mean(fit.fa$BUGSoutput$sims.list$ar)
@@ -62,5 +61,5 @@ mean(fit.fa$BUGSoutput$mean$ar)
 var(fit.fa$BUGSoutput$mean$ar)
 mean(fit.fa$BUGSoutput$mean$alpha)
 
-fit.fa
+
 
