43808edfac0d56d48378bc1d99241a06105001e5
Philipp-42 <95182086+Philipp-42@users.noreply.github.com>
2024-01-24 14:19:08 +0100

    added initial values

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-setwd("/Users/dennisperrett/Documents/Uni/Semester 5/Research Seminar/simulation_study_DSEM")
+setwd("/Users/philippholscher/Downloads/simulation_study_DSEM-main")
 
 library(mvtnorm)
 library(R2jags)
@@ -8,7 +8,7 @@ Sys.setenv(LANG = "en")
 source("gen_data_version02.R")
 source("utils.R")
 ##########################
-phi0 <- diag(1)*.7+.3 # cov(eta) # Per latent factor
+phi0 <- diag(1)*.7+.3# cov(eta) # Per latent factor
 mu0  <- c(0)          # mean(eta). # Mean of latent (deactivated)
 ar0  <- c(0.5)
 # ar(1) structure # Per Latent Factor
@@ -37,7 +37,11 @@ data <- list(
   J = J,
   observed_data = dat1
 )
-
+inits <- list(list(
+  ar = 0.5,  # Initial value for AR coefficient
+  ly = c(1,1,2)),list(
+    ar = 0.5,  # Initial value for AR coefficient
+    ly =c(1,1,2) ))
 #################################
 # Test run
 #################################
@@ -45,14 +49,15 @@ params <- c("ly","ar")
 
 #Note: Check working directory to find the model.file.
 fit.fa <- jags(data,  parameters.to.save=params, model.file="model1.txt", n.chains=2, n.iter=3000,
-               n.burnin = 500, n.thin=1)
+               n.burnin = 500, n.thin=1,inits = inits)
+
 
 
 fit.fa
 
 ########## Proper Stuff ##########
 
-N.List <- c(10,20,30,40,50)
+N.List <- c(50,40,30,20,10)
 T.List <- c(25, 50, 100)
 
 
@@ -63,8 +68,8 @@ T.List <- c(25, 50, 100)
 # We need at least 100 iterations of each. Philipp starts at 50 and works down.
 # Dennis starts at 10 and works up. When we have simmed enough, move on to the next N.
 # If we have 100 of each, do it again until we have 200 of each.
-# We may need a few more, if for some reason the N.Effs are low for some samples. In this case, we would just delete these. 
-run.models(reps=5, model.file = "model1.txt",N=10,NT=100)
+# We may need a few more, if for some reason the N.Effs are low for some samples. In this case, we would just delete these.
+run.models(reps=100, model.file = "model1.txt",N=50,NT=100)
 
 check.status("results_model1")
 
